567ceef21e8810e2cc1440b808767fdd06e81709
Roma Margine <rmargine@pic-informatica.it>
2024-12-17 21:50:57 +0100

    Stting up wix/ wix-client

---
+++ src/lib/wix-client.base.ts
@@ -0,0 +1,33 @@
+import { createClient, OAuthStrategy } from "@wix/sdk";
+import { env } from "@/env";
+import {
+    backInStockNotifications,
+    checkout,
+    currentCart,
+    orders,
+    recommendations,
+} from "@wix/ecom";
+import { files } from "@wix/media";
+import { members } from "@wix/members";
+import { redirects } from "@wix/redirects";
+import { collections, products } from "@wix/stores";
+
+export function getWixClient() {
+    return createClient({
+        modules: {
+            products,
+            collections,
+            currentCart,
+            checkout,
+            redirects,
+            orders,
+            recommendations,
+            backInStockNotifications,
+            members,
+            files,
+        },
+        auth: OAuthStrategy({
+            clientId: env.NEXT_PUBLIC_WIX_CIENT_ID,
+        }),
+    })
+}
