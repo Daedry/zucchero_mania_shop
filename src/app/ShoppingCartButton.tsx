89d81698e392bf38428eb532c5cb2588867a137b
Roma Margine <rmargine@pic-informatica.it>
2025-02-14 11:30:00 +0100

    Fix expect-error

--- src/app/ShoppingCartButton.tsx
+++ src/app/ShoppingCartButton.tsx
@@ -92,7 +92,7 @@ export default function ShoppingCartButton({
                     <div className="space-y-0.5">
                     <p className="text-sm">Importo totale:</p>
                     <p className="font-bold">
-                        {/* @ts-expect-error */}
+                        {/* @ts-expect-error Il tipo di subtotal potrebbe non essere definito in alcuni casi */}
                         {cartQuery.data?.subtotal?.formattedConvertedAmount}
                     </p>
                     <p className="text-xs text-muted-foreground">
