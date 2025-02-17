3556840db37f59fe8aea70813c2d821b05d88535
Roma Margine <rmargine@pic-informatica.it>
2025-02-17 17:11:38 +0100

    fix

--- src/app/collections/[slug]/layout.tsx
+++ src/app/collections/[slug]/layout.tsx
@@ -6,62 +6,65 @@ import { getCollectionBySlug } from "@/wix-api/collections";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
+
 // Il layout viene dichiarato come async
 export default async function Layout({
-  children,
-  params,
+    children,
+    params,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
   params: { slug: string }; // Assumiamo che slug esista sempre in una route dinamica
 }) {
-  if (!params.slug) notFound();
+    
+    const { slug } = await params;
+    if (!slug) notFound();
 
-  // Eseguiamo il fetching dei dati direttamente nel layout
-  const collection = await getCollectionBySlug(getWixServerClient(), params.slug);
-  if (!collection) notFound();
+    // Eseguiamo il fetching dei dati direttamente nel layout
+    const collection = await getCollectionBySlug(getWixServerClient(), slug);
+    if (!collection) notFound();
 
-  const banner = collection.media?.mainMedia?.image;
+    const banner = collection.media?.mainMedia?.image;
 
-  return (
-    <Suspense fallback={<LoadingSkeleton />}>
-      <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
-        <div className="flex flex-col gap-10">
-          {banner && (
-            <div className="relative hidden sm:block">
-              <WixImage
-                mediaIdentifier={banner.url}
-                width={1280}
-                height={400}
-                alt={banner.altText}
-              />
-              <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black" />
-              <h1 className="absolute bottom-10 left-1/2 -translate-x-1/2 text-4xl font-bold text-white lg:text-5xl">
+    return (
+        <Suspense fallback={<LoadingSkeleton />}>
+        <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
+            <div className="flex flex-col gap-10">
+            {banner && (
+                <div className="relative hidden sm:block">
+                <WixImage
+                    mediaIdentifier={banner.url}
+                    width={1280}
+                    height={400}
+                    alt={banner.altText}
+                />
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black" />
+                <h1 className="absolute bottom-10 left-1/2 -translate-x-1/2 text-4xl font-bold text-white lg:text-5xl">
+                    {collection.name}
+                </h1>
+                </div>
+            )}
+            <h1 className={cn("mx-auto text-3xl font-bold md:text-4xl", banner && "sm:hidden")}>
                 {collection.name}
-              </h1>
+            </h1>
             </div>
-          )}
-          <h1 className={cn("mx-auto text-3xl font-bold md:text-4xl", banner && "sm:hidden")}>
-            {collection.name}
-          </h1>
-        </div>
-        {children}
-      </main>
-    </Suspense>
-  );
+            {children}
+        </main>
+        </Suspense>
+    );
 }
 
 function LoadingSkeleton() {
-  return (
-    <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
-      <Skeleton className="mx-auto h-10 w-48 sm:block sm:aspect-[1280/400] sm:h-full sm:w-full" />
-      <div className="space-y-5">
-        <h2 className="text-2xl font-bold">Prodotti</h2>
-        <div className="flex grid-cols-2 flex-col gap-5 sm:grid md:grid-cols-3 lg:grid-cols-4">
-          {Array.from({ length: 8 }).map((_, i) => (
-            <Skeleton key={i} className="h-[26rem] w-full" />
-          ))}
+    return (
+        <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
+        <Skeleton className="mx-auto h-10 w-48 sm:block sm:aspect-[1280/400] sm:h-full sm:w-full" />
+        <div className="space-y-5">
+            <h2 className="text-2xl font-bold">Prodotti</h2>
+            <div className="flex grid-cols-2 flex-col gap-5 sm:grid md:grid-cols-3 lg:grid-cols-4">
+            {Array.from({ length: 8 }).map((_, i) => (
+                <Skeleton key={i} className="h-[26rem] w-full" />
+            ))}
+            </div>
         </div>
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
