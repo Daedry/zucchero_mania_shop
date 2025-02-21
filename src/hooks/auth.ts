3dcf5b6064219255632cd06cb9e0dad6401cde9c
Roma Margine <rmargine@pic-informatica.it>
2025-02-21 19:24:07 +0100

    Fix

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -4,7 +4,6 @@
     import Cookies from "js-cookie";
     import { usePathname } from "next/navigation";
     import { useToast } from "./use-toast";
-import { delay } from "@/lib/utils";
 
 export default function useAuth() {
     const pathname = usePathname();
